5b5bac336f6568accfd8f7c57df044691bed01f1
Palakgupta-123 <thegoldencravers@gmail.com>
2026-01-04 11:14:51 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function joinNow() {
+    alert("Welcome to Power Gym!");
+}
+
+function submitForm() {
+    const name = document.getElementById("name").value;
+    alert("Thank you " + name + ", we will contact you soon.");
+    return false;
+}
